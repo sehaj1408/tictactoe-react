1e5f5fe13c861acddf79496e62dfa9fbaa6d9b3a
Sehaj Naangal <snaangal@uwo.ca>
2023-01-10 13:11:34 -0500

    use Board to store state of each Square, add handleClick to set value when clicked

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    // add constructor to initialize the state
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-        // write () => because we are creating a function so it does not execute for every re-render
         <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
         >
-            { this.state.value }
+            { this.props.value }
         </button>
         );
     }
@@ -32,8 +23,19 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />
+        return (
+        <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+        />
+        );
     }
 
     render() {
