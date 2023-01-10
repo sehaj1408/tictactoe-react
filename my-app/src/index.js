2c8b01ff25e16c552bf0bc4f73340339d8d07f9f
Sehaj Naangal <snaangal@uwo.ca>
2023-01-10 12:26:06 -0500

    pass number prop from Board to Square using renderSquare

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,62 +4,62 @@ import './index.css';
 
 class Square extends React.Component {
     render() {
-      return (
+        return (
         <button className="square">
-          {/* TODO */}
+            { this.props.value }
         </button>
-      );
+        );
     }
-  }
+}
   
-  class Board extends React.Component {
+class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+        return <Square value={i} />
     }
-  
+
     render() {
-      const status = 'Next player: X';
-  
-      return (
+        const status = 'Next player: X';
+
+        return (
         <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
+            <div className="status">{status}</div>
+            <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
+            </div>
+            <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
+            </div>
+            <div className="board-row">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
-          </div>
+            </div>
         </div>
-      );
-    }
-  }
+        );
+        }
+}
   
-  class Game extends React.Component {
+class Game extends React.Component {
     render() {
-      return (
+        return (
         <div className="game">
-          <div className="game-board">
+            <div className="game-board">
             <Board />
-          </div>
-          <div className="game-info">
+            </div>
+            <div className="game-info">
             <div>{/* status */}</div>
             <ol>{/* TODO */}</ol>
-          </div>
+            </div>
         </div>
-      );
+        );
     }
-  }
+}
   
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
